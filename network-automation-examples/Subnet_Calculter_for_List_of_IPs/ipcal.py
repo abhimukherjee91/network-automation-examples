594996f71ef6ee8316ef2b7b7f9d2bfb367e1d2e
abhi.mukherjee <50944869+abhimukherjee91@users.noreply.github.com>
2021-07-30 17:03:17 +0530

    Subnet calculater for list of IPs

---
+++ network-automation-examples/Subnet_Calculter_for_List_of_IPs/ipcal.py
@@ -0,0 +1,29 @@
+#Auther: Abhi.Mukherjee
+
+import ipcalc
+
+list = []
+with open ("networks.csv",'r') as f:
+    for line in f:
+        list.append(line.strip())
+del list[0]
+with open ("networks_output.csv",'w') as f:
+    f.write("Subnet" + "\n")
+    f.close()
+for line in list:
+    subnet = ipcalc.Network(line)
+    
+    print(str(subnet.network()))
+    print(str(subnet.subnet()))
+    full_subnet = str(subnet.network()) + '/' + str(subnet.subnet())
+    line_to_pri = line + ',' + full_subnet
+    with open ("networks_output.csv",'a') as f:
+        f.write(line_to_pri + "\n")
+        f.close()
+
+Print("Completed Sucessfully")
+
+
+
+
+
